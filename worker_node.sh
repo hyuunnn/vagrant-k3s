ff7d9108190dcbef2d492387c6476eb7cf0e4173
Hyun Yi <hy00un__@naver.com>
2023-08-23 20:57:28 +0900

    worker node ip setting

--- worker_node.sh
+++ worker_node.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y docker.io nfs-common curl vim net-tools
 
 curl -sfL https://get.k3s.io | K3S_URL=https://$(cat /vagrant/master_ip):6443 \
     K3S_TOKEN=$(cat /vagrant/node-token) \
-    INSTALL_K3S_EXEC="--node-name $(sudo hostname) --docker" sh -s -
+    INSTALL_K3S_EXEC="--node-name $(sudo hostname) --docker --node-ip $(head -c -2 /vagrant/master_ip)$(sudo hostname | rev | cut -c 1)" sh -s -
