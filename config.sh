28a8ad646d659bd37652ad628068b55baccc1d1f
Hyun Yi <hy00un__@naver.com>
2023-09-03 10:18:43 +0900

    add `.vimrc`

--- config.sh
+++ config.sh
@@ -8,3 +8,10 @@ sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 # https://github.com/k3s-io/k3s/issues/60#issuecomment-467858023
 echo "192.168.123.120 master" >> /etc/hosts
 for (( i=1; i<=$1; i++  )); do echo "192.168.123.12$i worker$i" >> /etc/hosts; done
+
+# .vimrc
+echo "set nu" >> ~/.vimrc
+echo "set mouse=a" >> ~/.vimrc
+echo "set ignorecase" >> ~/.vimrc
+echo "set hlsearch" >> ~/.vimrc
+echo "set incsearch" >> ~/.vimrc
