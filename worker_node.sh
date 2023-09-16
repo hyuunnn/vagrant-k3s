beb047aeb7a885ff3e3ec361a9af53619c7153aa
Hyun Yi <hy00un__@naver.com>
2023-09-17 03:41:23 +0900

    add link

--- worker_node.sh
+++ worker_node.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y docker.io nfs-common curl vim net-tools
 
+# https://github.com/rgl/k3s-vagrant/blob/master/provision-k3s-server.sh
 curl -sfL https://get.k3s.io | K3S_URL=https://$(cat /vagrant/master_ip):6443 \
     K3S_TOKEN=$(cat /vagrant/node-token) \
     INSTALL_K3S_VERSION="v1.26.4+k3s1" \
